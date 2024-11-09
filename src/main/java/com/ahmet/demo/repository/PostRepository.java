e230a44d0c2ed1f3c5da704d05f0390f41fd92fe
drahmetacik <a.acik@icloud.com>
2024-11-09 10:29:29 +0000

    add repository for 4 tables

---
+++ src/main/java/com/ahmet/demo/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.ahmet.demo.repository;
+
+import com.ahmet.demo.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
